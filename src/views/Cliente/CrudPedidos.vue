0e18a67e51ce9c832c46547d41f015b9e5dbaf4f
diegoalz <diego.lunaza@uanl.edu.mx>
2022-11-07 14:18:23 -0600

    add create modal

--- src/views/Cliente/CrudPedidos.vue
+++ src/views/Cliente/CrudPedidos.vue
@@ -2,8 +2,42 @@
     <h1>Pagina para que el cliente relice pedidos</h1>
 
         <button class="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0 px-3 py-2 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded margin ">
-            <p class="text-sm font-medium leading-none text-white">Agregar pedidos</p>
+            <p class="text-sm font-medium leading-none text-white" @click="modalCrear=true">Agregar pedidos</p>
         </button>
+        <!-- Modal crear -->
+        <div class="py-12 bg-gray-700/50 z-10 absolute top-0 right-0 bottom-0 left-0" id="modal" v-show="modalCrear">
+        <div role="alert" class="container mx-auto w-11/12 md:w-2/3 max-w-lg">
+            <div class="relative py-8 px-5 md:px-10 bg-white shadow-md rounded border border-gray-400">
+                <div class="w-full flex justify-start text-gray-600 mb-3">
+
+                    <svg class="h-16 w-16 text-blue-500"  width="24"  height="24"  viewBox="0 0 24 24"  xmlns="http://www.w3.org/2000/svg"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M12 20h9" />  <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z" /></svg>
+
+
+                </div>
+                <h1 class="text-gray-800 font-lg font-bold tracking-normal leading-tight mb-4">Agregar un pedido</h1>
+                <label for="name" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Nombre</label>
+                <input id="name" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Ana Lisa Melchotto" />
+                <label for="name" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Descripción</label>
+                <input id="name" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Jugo de naranja" />
+                <label for="name" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Precio</label>
+                <input id="name" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="$50" />
+                <label for="name" class="text-gray-800 text-sm font-bold leading-tight tracking-normal">Estatus</label>
+                <input id="name" class="mb-5 mt-2 text-gray-600 focus:outline-none focus:border focus:border-indigo-700 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Activo" />
+
+                <div class="flex items-center justify-center w-full">
+                    <button class="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 bg-indigo-700 rounded text-white px-8 py-2 text-sm">Submit</button>
+                    <button class="focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-gray-400 ml-3 bg-gray-100 transition duration-150 text-gray-600 ease-in-out hover:border-gray-400 hover:bg-gray-300 border rounded px-8 py-2 text-sm" @click="modaledit=false" type="button">Cancel</button>
+                </div>
+                <button class="cursor-pointer absolute top-0 right-0 mt-4 mr-5 text-gray-400 hover:text-gray-600 rounded focus:ring-2 focus:outline-none focus:ring-gray-600" @click="modalCrear=false" type="button" aria-label="close modal" role="button">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  class="icon icon-tabler icon-tabler-x" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <line x1="18" y1="6" x2="6" y2="18" />
+                        <line x1="6" y1="6" x2="18" y2="18" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+    </div>
 
         <!-- component -->
         <div class="flex flex-col justify-center h-full">
@@ -173,6 +207,7 @@
             return {
                 modaledit: false,
                 modalborrar: false,
+                modalCrear: false,
             }
         }
     };
