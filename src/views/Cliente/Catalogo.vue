9552787ba762cb3e60aeff32ac4e6dc82dd486be
diegoalz <diego.lunaza@uanl.edu.mx>
2022-11-15 12:09:17 -0600

    Catalogo añadido

--- src/views/Cliente/Catalogo.vue
+++ src/views/Cliente/Catalogo.vue
@@ -6,26 +6,20 @@
                     <!-- Card 1 -->
                     <div tabindex="0" class="focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
                         <div>
-                            <img alt="person capturing an image" src="https://www.cardamomo.news/__export/1622218609247/sites/debate/img/2021/05/28/jugos_naturales_crop1622218346714.jpeg_1187729725.jpeg" tabindex="0" class="focus:outline-none w-full h-44" />
+                            <img alt="person capturing an image" src="https://recetasdebatidos.com/wp-content/uploads/2021/03/Jugo-de-Zanahoria.jpg" tabindex="0" class="focus:outline-none w-full h-44" />
                         </div>
                         <div class="bg-white">
                             <div class="flex items-center justify-between px-4 pt-4">
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" tabindex="0" class="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
-                                    </svg>
-                                </div>
                                 <div class="bg-yellow-200 py-1.5 px-6 rounded-full">
                                     <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 50%</p>
                                 </div>
                             </div>
                             <div class="p-4">
                                 <div class="flex items-center">
-                                    <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">Varios jugos</h2>
-                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$150 MX</p>
+                                    <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">Jugo de Zanahoria</h2>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$42.99 MX</p>
                                 </div>
-                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Paquete de varias jugos y aguas frescas</p>
+                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Presentación de un litro</p>
                                 <div class="flex items-center justify-between py-4">
                                     <h2 tabindex="0" class="focus:outline-none text-indigo-700 text-xs font-semibold">Jugoson San Nicolas</h2>
                                     <h3 tabindex="0" class="focus:outline-none text-indigo-700 text-xl font-semibold"></h3>
@@ -41,22 +35,16 @@
                         </div>
                         <div class="bg-white">
                             <div class="flex items-center justify-between px-4 pt-4 " >
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" tabindex="0" class="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
-                                    </svg>
-                                </div>
                                 <div class="bg-yellow-200 py-1.5 px-6 rounded-full">
-                                    <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 50%</p>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 25%</p>
                                 </div>
                             </div>
                             <div class="p-4">
                                 <div class="flex items-center">
                                     <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">Jugo de limón</h2>
-                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$150 MX</p>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$53.21 MX</p>
                                 </div>
-                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Paquete de varios jugos de limón, usted elige la cantidad deseada.</p>
+                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Presentación de un litro</p>
                                 <div class="flex items-center justify-between py-4">
                                     <h2 tabindex="0" class="focus:outline-none text-indigo-700 text-xs font-semibold">Jugoson San Nicolas</h2>
                                     <h3 tabindex="0" class="focus:outline-none text-indigo-700 text-xl font-semibold"></h3>
@@ -72,22 +60,16 @@
                         </div>
                         <div class="bg-white">
                             <div class="flex items-center justify-between px-4 pt-4">
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" tabindex="0" class="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
-                                    </svg>
-                                </div>
                                 <div class="bg-yellow-200 py-1.5 px-6 rounded-full">
-                                    <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 50%</p>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 10%</p>
                                 </div>
                             </div>
                             <div class="p-4">
                                 <div class="flex items-center">
                                     <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">Jugo de naranja</h2>
-                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$150 MX</p>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$45.88 MX</p>
                                 </div>
-                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Selección de jugos de naranja, cantidad al gusto.</p>
+                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Presentación de un litro</p>
                                 <div class="flex items-center justify-between py-4">
                                     <h2 tabindex="0" class="focus:outline-none text-indigo-700 text-xs font-semibold">Jugoson San Nicolas</h2>
                                     <h3 tabindex="0" class="focus:outline-none text-indigo-700 text-xl font-semibold"></h3>
@@ -99,26 +81,20 @@
                     <!-- Card 4  -->
                     <div tabindex="0" class="focus:outline-none mx-2 w-72 xl:mb-0 mb-8">
                         <div>
-                            <img alt="person capturing an image" src="https://cdn7.kiwilimon.com/recetaimagen/31783/36556.jpg" tabindex="0" class="focus:outline-none w-full h-44" />
+                            <img alt="person capturing an image" src="https://diacomer.com/wp-content/uploads/2022/04/NaranjaPremiumSinPulpaGalo%CC%81n.png" tabindex="0" class="focus:outline-none w-full h-44" />
                         </div>
                         <div class="bg-white">
                             <div class="flex items-center justify-between px-4 pt-4">
-                                <div>
-                                    <svg xmlns="http://www.w3.org/2000/svg" tabindex="0" class="focus:outline-none" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"></path>
-                                    </svg>
-                                </div>
                                 <div class="bg-yellow-200 py-1.5 px-6 rounded-full">
-                                    <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 50%</p>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-yellow-700">Descuento 6%</p>
                                 </div>
                             </div>
                             <div class="p-4">
                                 <div class="flex items-center">
-                                    <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">Agua de jamaica</h2>
-                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$150 MX</p>
+                                    <h2 tabindex="0" class="focus:outline-none text-lg font-semibold">Galon de naranja</h2>
+                                    <p tabindex="0" class="focus:outline-none text-xs text-gray-600 pl-5">$170 MX</p>
                                 </div>
-                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Selección de aguas frescas de estracto de jamaica.</p>
+                                <p tabindex="0" class="focus:outline-none text-xs text-gray-600 mt-2 text-left">Presentación de 1 galon(3.7 litros)</p>
                                 <div class="flex items-center justify-between py-4">
                                     <h2 tabindex="0" class="focus:outline-none text-indigo-700 text-xs font-semibold">Jugoson San Nicolas</h2>
                                     <h3 tabindex="0" class="focus:outline-none text-indigo-700 text-xl font-semibold"></h3>
